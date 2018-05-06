13c6e22d3e5722a38561c3285e5651228680e43d
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-05-06 17:45:46 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -8,13 +8,9 @@ SVG file that can be opened in your default browser.
 
 You can get it by downloading it directly or by typing:
 
-.. code:: bash
-
     $ pip install BitcoinValueGraph
 
 After it is installed you can start it by simply typing in your terminal:
-
-.. code:: bash
 
     $ bitcoin
 
